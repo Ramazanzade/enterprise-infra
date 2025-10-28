3f477617e69a7bbffa5a734ce2042b7761f93806
Nizam Ramazanzade <ramazanov570633@gmail.com>
2025-10-28 17:05:33 +0400

    main fix 5

--- variables.tf
+++ variables.tf
@@ -17,19 +17,19 @@ variable "vpc_cidr" {
 }
 
 variable "azs" {
-  description = "Availability Zones"
+  description = "List of availability zones"
   type        = list(string)
   default     = ["eu-central-1a", "eu-central-1b"]
 }
 
-variable "public_cidrs" {
-  description = "Public subnet CIDRs"
+variable "public_subnets_cidrs" {
+  description = "List of public subnet CIDRs"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "private_cidrs" {
-  description = "Private subnet CIDRs"
+variable "private_subnets_cidrs" {
+  description = "List of private subnet CIDRs"
   type        = list(string)
-  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
 }
