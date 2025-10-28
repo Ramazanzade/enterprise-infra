a8b2b7519f26606a0303863aab922d56c3bc3d64
Nizam Ramazanzade <ramazanov570633@gmail.com>
2025-10-28 16:38:28 +0400

    main fix 2

--- security/main.tf
+++ security/main.tf
@@ -1,1 +1,51 @@
-salam
+#####################################
+# Security Groups
+#####################################
+
+resource "aws_security_group" "public" {
+  name   = "${var.project}-public-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-public-sg"
+    Tier = "public"
+  }
+}
+
+resource "aws_security_group" "private" {
+  name   = "${var.project}-private-sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.public.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project}-private-sg"
+    Tier = "private"
+  }
+}
