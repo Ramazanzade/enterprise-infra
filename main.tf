e1b1f0c05463b2b2a44c5a83c5210d62e6b45291
Nizam Ramazanzade <ramazanov570633@gmail.com>
2025-11-03 16:30:20 +0400

    root main update 4

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ module "security" {
 
 #####################################
 module "ec2" {
-  source             = "./modules/ec2"
+  source             = "./ec2"
   project            = var.project
   vpc_id             = module.vpc.vpc_id           # ✅ yalnız burda
   public_subnet_id   = element(module.vpc.public_subnets, 0)
