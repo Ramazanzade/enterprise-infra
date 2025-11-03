7246e5c4f2d6ac83e9e65561dab9ae8b8444951d
Nizam Ramazanzade <ramazanov570633@gmail.com>
2025-11-03 16:23:52 +0400

    security and ec2 fix update

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -34,3 +34,8 @@ variable "public_key_path" {
   type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
+
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+}
